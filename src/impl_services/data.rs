595c97584d494ec133a6b49b7d57f2af98227c70
tkoike <tkoike@easter.kuee.kyoto-u.ac.jp>
2024-10-11 16:36:47 +0900

    fix the wrong var

--- src/impl_services/data.rs
+++ src/impl_services/data.rs
@@ -1331,7 +1331,7 @@ fn generate_read(
 
     let start: usize = match sample_info.is_amplicon {
         true => 0,
-        false => rng.geen_range(0..file_info.contig_len - 1000),
+        false => rng.gen_range(0..file_info.contig_len - 1000),
     };
     // Get our distribution from either the Sample specified Gamma or the global read length
     let read_distribution = &sample_info.read_len_dist;
